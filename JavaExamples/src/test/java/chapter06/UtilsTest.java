ee241a3f14834823416e109a7a9a78dd37347474
Emily <emily.zy.lin@gmail.com>
2025-02-20 21:19:16 -0600

    UtilsTest chapter6

--- JavaExamples/src/test/java/chapter06/UtilsTest.java
+++ JavaExamples/src/test/java/chapter06/UtilsTest.java
@@ -1,9 +1,18 @@
 package chapter06;
 
 // TODO: Add imports for JUnit 5
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UtilsTest {
 
     // TODO: Write a test for getInitials()
+    @Test
+    public void checkGetInitials() {
+        String msg = "getInitials() should only return initials";
+        String expected = "GH";
+        String actual = Utils.getInitials("Gordon Hens");
+        assertEquals(expected, actual, msg);
+    }
 
 }
